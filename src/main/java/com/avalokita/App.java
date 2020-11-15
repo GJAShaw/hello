106dd524cb951e8ecb303139ab1108abd2762f44
Greg Shaw <g.j.a.shaw@gmail.com>
2020-11-15 19:08:17 +0000

    Implement log4j2 logging
    - create and populate src/main/resources/log4j2.xml
    - add log4j-core and log4j-api dependencies to pom.xml
    - import LogManager and Logger in Java source
    - replace System.out.println() with Logger methods

--- src/main/java/com/avalokita/App.java
+++ src/main/java/com/avalokita/App.java
@@ -1,13 +1,19 @@
 package com.avalokita;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
+    private static final Logger LOG = LogManager.getLogger(App.class);
+    
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        LOG.info( "Hello World!" );
     }
 }
